2cb730d370d640b88348d92ca286535533f8968d
wangkai <wangkai@yingmi.cn>
2019-05-05 14:55:55 +0800

    refine delegation.

--- XPresentation/Utils+UIKit.swift
+++ XPresentation/Utils+UIKit.swift
@@ -98,135 +98,138 @@ public final class PresentationWindow: UIWindow {
     }
     
     @discardableResult
-    public func presentTransparentRootNav(_ nav: UINavigationController? = nil,
-                                          root: UIViewController,
-                                          push vc: UIViewController,
-                                          async: Bool = true, animated: Bool = true, completion: (() -> Void)? = nil) -> UIWindow {
-        return presentTransparentRootNav(nav, root: root, push: {nav, root in nav.pushViewController(vc, animated: animated)}, async: async, completion: completion)
+    public func presentTransparentRootNav<Nav: UINavigationController>(
+        _ nav: Nav,
+        root: UIViewController,
+        delegation: (Nav, PresentationNavDelegate) -> Void = { $0.delegate = $1 },
+        push vc: UIViewController,
+        async: Bool = true, animated: Bool = true, completion: (() -> Void)? = nil) -> UIWindow {
+        
+        return presentTransparentRootNav(nav, root: root, delegation: delegation, push: {nav, root in nav.pushViewController(vc, animated: animated)}, async: async, completion: completion)
     }
     
     @discardableResult
-    public func presentTransparentRootNav(_ nav: UINavigationController? = nil,
-                                          root: UIViewController,
-                                          push action: @escaping (UINavigationController, UIViewController) -> Void,
-                                          async: Bool = true, completion: (() -> Void)? = nil) -> UIWindow {
-        final class Delegate: NSObject, UINavigationControllerDelegate {
-            static var Key = "Presentation.NavigationControllerDelegate.key"
-            
-            private weak var outerDelegate: UINavigationControllerDelegate?
-            private var didPush = false
-            private var pushCompletion: (() -> Void)?
-            
-            init(_ delegate: UINavigationControllerDelegate?, pushCompletion: (() -> Void)?) {
-                self.outerDelegate = delegate
-            }
-            
-            private var distinct: [String: Bool] = [:]
-            //
-            func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-                guard !(distinct[#function] ?? false) else { return }
-                distinct[#function] = true
-                defer { distinct[#function] = false }
-                outerDelegate?.navigationController?(navigationController, willShow: viewController, animated: animated)
-                
-                guard let root = navigationController.viewControllers.first else { return }
-                guard let coo = navigationController.transitionCoordinator, animated else { return }
-                
-                let transitionView = navigationController.navigationBar
-                if viewController === root {
-                    if didPush {
-                        root.view.alpha = 1
-                        coo.animate(
-                            alongsideTransition: { _ -> Void in
-                                transitionView.transform = CGAffineTransform(translationX: transitionView.frame.width, y: 0)
-                                root.view.alpha = 0
-                        },
-                            completion: { context -> Void in
-                                if context.isCancelled {
-                                    transitionView.transform = .identity
-                                    root.view.alpha = 1
-                                }
-                        })
-                    } else {
-                        transitionView.transform = CGAffineTransform(translationX: transitionView.frame.width, y: 0)
-                        root.view.alpha = 0
-                    }
-                } else if navigationController.viewControllers.count == 2, !didPush {
-                    coo.animate(
-                        alongsideTransition: { _ -> Void in
-                            transitionView.transform = .identity
-                            root.view.alpha = 1
-                    },
-                        completion: { context -> Void in
-                            if context.isCancelled {
-                                transitionView.transform = CGAffineTransform(translationX: transitionView.frame.width, y: 0)
-                                root.view.alpha = 0
-                            }
-                    })
-                }
-            }
-            
-            func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-                guard !(distinct[#function] ?? false) else { return }
-                distinct[#function] = true
-                defer { distinct[#function] = false }
-                outerDelegate?.navigationController?(navigationController, didShow: viewController, animated: animated)
-                
-                guard let root = navigationController.viewControllers.first else { return }
-                if didPush && viewController === root {
-                    navigationController.dismiss(animated: false, completion: nil)
-                }
-                if !didPush && viewController !== root {
-                    didPush = true
-                    pushCompletion?()
-                    pushCompletion = nil
-                }
-            }
-            
-            //
-            private let UINavigationControllerDelegateSelectors: Set<Selector> = {
-                var count: UInt32 = 0
-                let protocolMethods = protocol_copyMethodDescriptionList(UINavigationControllerDelegate.self, false, true, &count)
-                let selectors = UnsafeMutableBufferPointer(start: protocolMethods, count: Int(count))
-                    .compactMap{ $0.name }
-                free(protocolMethods)
-                return Set<Selector>(selectors)
-            }()
-            
-            override func responds(to aSelector: Selector!) -> Bool {
-                switch aSelector {
-                case #selector(UINavigationControllerDelegate.navigationController(_:willShow:animated:)),
-                     #selector(UINavigationControllerDelegate.navigationController(_:didShow:animated:)):
-                    return true
-                default:
-                    
-                    return UINavigationControllerDelegateSelectors.contains(aSelector) ? (outerDelegate?.responds(to: aSelector) ?? false) : super.responds(to: aSelector)
-                }
-            }
-            
-            override func forwardingTarget(for aSelector: Selector!) -> Any? {
-                return UINavigationControllerDelegateSelectors.contains(aSelector) ? outerDelegate : super.forwardingTarget(for: aSelector)
-            }
-        }
+    public func presentTransparentRootNav<Nav: UINavigationController, Root: UIViewController>(
+        _ nav: Nav,
+        root: Root,
+        delegation: (Nav, PresentationNavDelegate) -> Void = { $0.delegate = $1 },
+        push action: @escaping (Nav, Root) -> Void,
+        async: Bool = true, completion: (() -> Void)? = nil) -> UIWindow {
         
         // adjust root
         root.view.backgroundColor = UIColor(white: 0, alpha: 0.1)
         root.view.alpha = 0
         
         // config nav
-        let nav = nav ?? UINavigationController()
         nav.viewControllers = [root]
-        let delegate = Delegate(nav.delegate, pushCompletion: completion)
-        nav.delegate = delegate
-        objc_setAssociatedObject(nav, &Delegate.Key, delegate, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        let defaultDelegate = PresentationNavDelegate()
+        defaultDelegate.pushCompletion = completion
+        delegation(nav, defaultDelegate)
+        objc_setAssociatedObject(nav, &PresentationNavDelegate.Key, defaultDelegate, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         
         // finally, do present without animation & push
-        present(async: async) {
-            $0.present(nav, animated: false) {
+        present(async: async, action: {
+            $0.present(nav, animated: false, completion: {
                 action(nav, root)
-            }
-        }
+            })
+        })
         
         return self
+    }
+}
+
+
+public final class PresentationNavDelegate: NSObject, UINavigationControllerDelegate {
+    static var Key = "Presentation.NavigationControllerDelegate.key"
+    
+    public weak var outerDelegate: UINavigationControllerDelegate?
+    
+    fileprivate var pushCompletion: (() -> Void)?
+    private var didPush = false
+    private var distinct: [String: Bool] = [:]
+    //
+    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        guard !(distinct[#function] ?? false) else { return }
+        distinct[#function] = true
+        defer { distinct[#function] = false }
+        outerDelegate?.navigationController?(navigationController, willShow: viewController, animated: animated)
+        
+        guard let root = navigationController.viewControllers.first else { return }
+        guard let coo = navigationController.transitionCoordinator, animated else { return }
+        
+        let transitionView = navigationController.navigationBar
+        if viewController === root {
+            if didPush {
+                root.view.alpha = 1
+                coo.animate(
+                    alongsideTransition: { _ -> Void in
+                        transitionView.transform = CGAffineTransform(translationX: transitionView.frame.width, y: 0)
+                        root.view.alpha = 0
+                },
+                    completion: { context -> Void in
+                        if context.isCancelled {
+                            transitionView.transform = .identity
+                            root.view.alpha = 1
+                        }
+                })
+            } else {
+                transitionView.transform = CGAffineTransform(translationX: transitionView.frame.width, y: 0)
+                root.view.alpha = 0
+            }
+        } else if navigationController.viewControllers.count == 2, !didPush {
+            coo.animate(
+                alongsideTransition: { _ -> Void in
+                    transitionView.transform = .identity
+                    root.view.alpha = 1
+            },
+                completion: { context -> Void in
+                    if context.isCancelled {
+                        transitionView.transform = CGAffineTransform(translationX: transitionView.frame.width, y: 0)
+                        root.view.alpha = 0
+                    }
+            })
+        }
+    }
+    
+    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard !(distinct[#function] ?? false) else { return }
+        distinct[#function] = true
+        defer { distinct[#function] = false }
+        outerDelegate?.navigationController?(navigationController, didShow: viewController, animated: animated)
+        
+        guard let root = navigationController.viewControllers.first else { return }
+        if didPush && viewController === root {
+            navigationController.dismiss(animated: false, completion: nil)
+        }
+        if !didPush && viewController !== root {
+            didPush = true
+            pushCompletion?()
+            pushCompletion = nil
+        }
+    }
+    
+    //
+    private let UINavigationControllerDelegateSelectors: Set<Selector> = {
+        var count: UInt32 = 0
+        let protocolMethods = protocol_copyMethodDescriptionList(UINavigationControllerDelegate.self, false, true, &count)
+        let selectors = UnsafeMutableBufferPointer(start: protocolMethods, count: Int(count))
+            .compactMap{ $0.name }
+        free(protocolMethods)
+        return Set<Selector>(selectors)
+    }()
+    
+    public override func responds(to aSelector: Selector!) -> Bool {
+        switch aSelector {
+        case #selector(UINavigationControllerDelegate.navigationController(_:willShow:animated:)),
+             #selector(UINavigationControllerDelegate.navigationController(_:didShow:animated:)):
+            return true
+        default:
+            
+            return UINavigationControllerDelegateSelectors.contains(aSelector) ? (outerDelegate?.responds(to: aSelector) ?? false) : super.responds(to: aSelector)
+        }
+    }
+    
+    public override func forwardingTarget(for aSelector: Selector!) -> Any? {
+        return UINavigationControllerDelegateSelectors.contains(aSelector) ? outerDelegate : super.forwardingTarget(for: aSelector)
     }
 }
